cfca9fe9a70c5ad57995ae971bd1eb70800edd4f
Jas Chen <jaschen.tw@gmail.com>
2020-01-30 23:19:49 +0800

    fix hash check

--- src/btn.css.json.js
+++ src/btn.css.json.js
@@ -2,7 +2,7 @@ const PRIMARY_COLOR = '#007bff';
 
 test(__filename, async () => {
   await css`
-    .btn {
+    .primary-btn {
       cursor: pointer;
       color: #fff;
       background-color: ${PRIMARY_COLOR};
@@ -11,7 +11,10 @@ test(__filename, async () => {
       border: 1px solid ${PRIMARY_COLOR};
       padding: .375rem .75rem;
       border-radius: .25rem;
-      transition: background-color .15s ease-in-out,border-color .15s ease-in-out;
+      transition:
+        color .15s ease-in-out,
+        background-color .15s ease-in-out,
+        border-color .15s ease-in-out;
 
       &:hover {
         background-color: #0069d9;
@@ -20,13 +23,46 @@ test(__filename, async () => {
     }
 
     @media (min-width: 501px) {
-      .btn {
+      .primary-btn {
         display: inline-block;
       }
     }
 
     @media (max-width: 500px) {
-      .btn {
+      .primary-btn {
+        display: block;
+      }
+    }
+
+    .light-btn {
+      cursor: pointer;
+      color: #212529;
+      background-color: #f8f9fa;
+      text-align: center;
+      user-select: none;
+      border: 1px solid #f8f9fa;
+      padding: .375rem .75rem;
+      border-radius: .25rem;
+      transition:
+        color .15s ease-in-out,
+        background-color .15s ease-in-out,
+        border-color .15s ease-in-out;
+
+      &:hover {
+        color: #212529;
+        background-color: #e2e6ea;
+        border-color: #dae0e5;
+      }
+    }
+
+    @media (min-width: 501px) {
+      .light-btn {
+        display: inline-block;
+      }
+    }
+
+    @media (max-width: 500px) {
+      .light-btn {
         display: block;
       }
     }
